f9a2c34395348822615e8dbcdceda5235da384f8
Thomas6A <thomas.baudrin@outlook.fr>
2024-01-30 10:13:33 +0100

    feat(back): add notfication entity

---
+++ DockerEnvironnement/backend/src/main/java/com/company/project/entity/Email.java
@@ -0,0 +1,60 @@
+package com.company.project.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "email")
+public class Email {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	
+	@Column(nullable = false)
+	private String message;
+	
+	@Column(nullable = false)
+	private Commande commande;
+	
+	@Column(nullable = false)
+	private Utilisateur utilisateur;
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public Commande getCommande() {
+		return commande;
+	}
+
+	public void setCommande(Commande commande) {
+		this.commande = commande;
+	}
+
+	public Utilisateur getUtilisateur() {
+		return utilisateur;
+	}
+
+	public void setUtilisateur(Utilisateur utilisateur) {
+		this.utilisateur = utilisateur;
+	}
+	
+	
+	
+}
